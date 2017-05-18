d7d41868f9dc3ab48a9052d22e139e33d6d67457
Florian Dobener <flo@schroedingerscat.org>
2017-05-18 20:32:39 +0200

    Corrected the test case and updated README

--- tdm_loader_test/tests.py
+++ tdm_loader_test/tests.py
@@ -103,12 +103,12 @@ class TestTDMLoader(TestCase):
     def test_channel_dict(self):
         self.assertTrue((self.data.channel_dict(0)['Integer32_with_max_min'] == s.array([9, 10, 11, -50, sys.maxint, -sys.maxint - 1])).all())
         self.assertTrue((self.data.channel_dict(0)['Float as Float'] == s.array([.1, .2, .3, .4, .5, .6])).all())
-        self.assertTrue((self.data.channel_dict(0)['Float_4_integers'] == s.array([1, 2, 3, 4])).all())
+        self.assertTrue((self.data.channel_dict(0)['Float_4_Integers'] == s.array([1, 2, 3, 4])).all())
 
         self.assertTrue((self.data.channel_dict(1)[''] == s.array([0])).all())
 
-        self.assertRaises(IndexError, self.data.channel_dict(-1))
-        self.assertRaises(IndexError, self.data.channel_dict(2))
+        self.assertRaises(IndexError, self.data.channel_dict, -1)
+        self.assertRaises(IndexError, self.data.channel_dict, 2)
 
     def test_channel_unit(self):
         self.assertEqual(self.data.channel_unit(0, 0), "arb. units")
